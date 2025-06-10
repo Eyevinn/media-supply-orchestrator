61d73ed681d9f8605cb0c17549ff2fa393ba5c21
Jonas Birmé <jonas.birme@eyevinn.se>
2025-06-11 00:14:41 +0200

    feat: create transcode job when file is created

---
+++ src/orchestrator.ts
@@ -0,0 +1,51 @@
+import { Context } from '@osaas/client-core';
+import { setupListener } from './storage/minio';
+import { createUniqueSlug } from './util';
+import { transcode } from '@osaas/client-transcode';
+
+export interface OrchestratorOptions {
+  inputBucket: string;
+  abrsubsBucket: string;
+  encoreUrl: string;
+  s3EndpointUrl: string;
+  s3AccessKeyId: string;
+  s3SecretAccessKey: string;
+}
+
+export default (opts: OrchestratorOptions) => {
+  const handleCreateNotification = async (record: any): Promise<void> => {
+    console.log(
+      `File created: s3://${record.s3.bucket.name}/${record.s3.object.key}`
+    );
+    const filename =
+      record.s3.object.key.split('/').pop() || record.s3.object.key;
+    const externalId = createUniqueSlug(filename);
+
+    const ctx = new Context();
+    const encoreServiceAccessToken = await ctx.getServiceAccessToken('encore');
+    const job = await transcode(
+      ctx,
+      {
+        encoreInstanceName: 'mediasupply',
+        profile: 'program',
+        externalId,
+        inputUrl: new URL(
+          `s3://${record.s3.bucket.name}/${record.s3.object.key}`
+        ),
+        outputUrl: new URL(`s3://${opts.abrsubsBucket}/${externalId}/`)
+      },
+      {
+        endpointUrl: new URL(opts.encoreUrl),
+        bearerToken: encoreServiceAccessToken
+      }
+    );
+    console.log(job);
+  };
+  setupListener(
+    opts.inputBucket,
+    new URL(opts.s3EndpointUrl),
+    opts.s3AccessKeyId,
+    opts.s3SecretAccessKey,
+    handleCreateNotification
+  );
+};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,44 @@
 import api from './api';
+import orchestrator from './orchestrator';
 
-const server = api({ title: '@eyevinn/typescript-nodejs' });
+const server = api({ title: 'media-supply-orchestrator' });
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
+
+const INPUT_BUCKET = process.env.INPUT_BUCKET || 'input';
+const ABRSUBS_BUCKET = process.env.ABRSUBS_BUCKET || 'abrsubs';
+const OUTPUT_BUCKET = process.env.OUTPUT_BUCKET || 'origin';
+
+if (!INPUT_BUCKET || !ABRSUBS_BUCKET || !OUTPUT_BUCKET) {
+  throw new Error(
+    'Please set INPUT_BUCKET, ABRSUBS_BUCKET, and OUTPUT_BUCKET environment variables'
+  );
+}
+
+if (!process.env.S3_ENDPOINT_URL) {
+  throw new Error('Please set S3_ENDPOINT_URL environment variable');
+}
+
+if (!process.env.S3_ACCESS_KEY_ID) {
+  throw new Error('Please set S3_ACCESS_KEY_ID environment variable');
+}
+
+if (!process.env.S3_SECRET_ACCESS_KEY) {
+  throw new Error('Please set S3_SECRET_ACCESS_KEY environment variable');
+}
+
+if (!process.env.ENCORE_URL) {
+  throw new Error('Please set ENCORE_URL environment variable');
+}
+
+orchestrator({
+  inputBucket: INPUT_BUCKET,
+  abrsubsBucket: ABRSUBS_BUCKET,
+  encoreUrl: process.env.ENCORE_URL,
+  s3EndpointUrl: process.env.S3_ENDPOINT_URL,
+  s3AccessKeyId: process.env.S3_ACCESS_KEY_ID,
+  s3SecretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+});
 
 server.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) {
